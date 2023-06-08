b70f508965f3715acb0094798eb107719284fb51
LeooGSantos <rm98912@fiap.com.br>
2023-06-07 21:08:05 -0300

    js arrumado

--- js/incri-doa.js
+++ js/incri-doa.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var doacao1 = document.getElementById('doacao1').value;
       var doacao2 = document.getElementById('doacao2').value;
       var disponibilidade = document.getElementById('disponibilidade').value;
-      var tipoAlimento = document.getElementById('tipoAlimento').value;
+      var tipo = document.getElementById('tipo').value;
       var horario = document.getElementById('horario').value;
       var informacoes = document.getElementById('informacoes').value;
   
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log('Por que você decidiu fazer essa doação? ' + doacao1);
       console.log('Você já fez uma doação de alimentos alguma vez? ' + doacao2);
       console.log('Teria disponibilidade para ser voluntariado em atividades relacionadas à doação de alimentos? ' + disponibilidade);
-      console.log('Qual tipo de alimento você está doando? ' + tipoAlimento);
+      console.log('Qual tipo de alimento você está doando? ' + tipo);
       console.log('Qual o melhor horário para retirada ou entrega de doações de alimentos? ' + horario);
       console.log('Gostaria de receber informações sobre o destino das doações de alimentos? ' + informacoes);
   
