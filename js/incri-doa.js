0b208a715e549821ef8d8dae5effe9bdefd29ce6
LeooGSantos <rm98912@fiap.com.br>
2023-06-07 21:01:44 -0300

    adicionado campos novos no java scrpit

--- js/incri-doa.js
+++ js/incri-doa.js
@@ -10,6 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
       var email = document.getElementById('email').value;
       var endereco = document.getElementById('endereco').value;
       var alimento = document.getElementById('alimento').value;
+      var doacao1 = document.getElementById('doacao1').value;
+      var doacao2 = document.getElementById('doacao2').value;
+      var disponibilidade = document.getElementById('disponibilidade').value;
+      var tipoAlimento = document.getElementById('tipoAlimento').value;
+      var horario = document.getElementById('horario').value;
+      var informacoes = document.getElementById('informacoes').value;
   
       // Aqui você pode realizar qualquer ação desejada com os valores do formulário
       // Por exemplo, enviar os dados para um servidor ou realizar validações adicionais
@@ -19,9 +25,14 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log('E-mail: ' + email);
       console.log('Endereço: ' + endereco);
       console.log('Alimento: ' + alimento);
+      console.log('Por que você decidiu fazer essa doação? ' + doacao1);
+      console.log('Você já fez uma doação de alimentos alguma vez? ' + doacao2);
+      console.log('Teria disponibilidade para ser voluntariado em atividades relacionadas à doação de alimentos? ' + disponibilidade);
+      console.log('Qual tipo de alimento você está doando? ' + tipoAlimento);
+      console.log('Qual o melhor horário para retirada ou entrega de doações de alimentos? ' + horario);
+      console.log('Gostaria de receber informações sobre o destino das doações de alimentos? ' + informacoes);
   
       // Limpe o formulário após o envio
       form.reset();
     });
   });
-  
